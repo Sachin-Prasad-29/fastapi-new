83f9b81dc561c81dc3c89765411e73c800779a22
Sachin Prasad <sachin.prasad@xlscout.com>
2026-08-19 16:17:33 +0530

    Delete api added

--- main.py
+++ main.py
@@ -169,3 +169,16 @@ def update_patient(patient_id:str,patient_update:PatientUpdate):
     return JSONResponse(
         status_code=200, content={"message": "Patient updated successfully"}
     )
+
+
+@app.delete("/delete/{patient_id}")
+def delete_patient(patient_id:str):
+    data = load_patients()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404,detail="Patient Not Found")
+    
+    del data[patient_id]
+    save_data(data)
+
+    return JSONResponse(status_code=200,content={'message':"Patient Deleted Successfully"})
